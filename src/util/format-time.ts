0edca887ba32713678097d10203ffe6a95c2e514
Double Colon <djdavid98@protonmail.com>
2024-10-27 00:41:51 +0200

    Add hours to time formatting

--- src/util/format-time.ts
+++ src/util/format-time.ts
@@ -1,6 +1,7 @@
 import { DurationFormat } from '@formatjs/intl-durationformat';
 
 const dfCache: Record<string, DurationFormat> = {};
+const hourInSeconds = 60 * 60;
 
 export const formatTime = (locale: string, secondsFloat: number): string => {
   if (!(locale in dfCache)) {
@@ -8,10 +9,14 @@ export const formatTime = (locale: string, secondsFloat: number): string => {
   }
   const df = dfCache[locale];
   let timeTally = secondsFloat;
+  const hours = Math.floor(timeTally / hourInSeconds);
+  if (hours > 0) {
+    timeTally -= hours * hourInSeconds;
+  }
   const minutes = Math.floor(timeTally / 60);
   timeTally -= minutes * 60;
   const seconds = Math.floor(timeTally);
   timeTally -= seconds;
   const milliseconds = Math.round(1e3 * timeTally);
-  return df.format({ minutes, seconds, milliseconds });
+  return df.format({ hours, minutes, seconds, milliseconds });
 };
